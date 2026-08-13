66e709823ed40bf4a2a9bed835f8d24ed5b13616
enyonyiabemba94-eng <enyonyiabemba94@gmail.com>
2026-08-13 15:32:43 +0300

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -1,9 +1,2 @@
-/*
-  AFRN SUPABASE CONFIG
-
-  WEKA hapa URL na ANON/PUBLISHABLE KEY ya Supabase yako iliyopo.
-
-  Usitumie service_role key kwenye website.
-*/
-window.AFRN_SUPABASE_URL = "WEKA_SUPABASE_URL_YAKO_HAPA";
-window.AFRN_SUPABASE_ANON_KEY = "WEKA_SUPABASE_ANON_KEY_YAKO_HAPA";
+window.AFRN_SUPABASE_URL = "https://jjqhvruppafpumcthmwe.supabase.co";
+window.AFRN_SUPABASE_ANON_KEY = sb_publishable_02hhRG8bgDOqSFxva8IMvQ_zWTLMa3G
