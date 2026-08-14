b1ea35b358ca4ad593b0243f6beea4c1556ffdfd
enyonyiabemba94-eng <enyonyiabemba94@gmail.com>
2026-08-14 15:13:09 +0300

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -1,3 +1,9 @@
 window.AFRN_SUPABASE_URL = "https://jjqhvruppafpumcthmwe.supabase.co";
 
 window.AFRN_SUPABASE_ANON_KEY = "sb_publishable_02hhRG8bgDOqSFxva8IMvQ_zWTLMa3G";
+
+window.supabaseClient =
+  window.supabase.createClient(
+    window.AFRN_SUPABASE_URL,
+    window.AFRN_SUPABASE_ANON_KEY
+  );
