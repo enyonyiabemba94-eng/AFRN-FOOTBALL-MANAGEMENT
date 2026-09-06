472279c64061c7bf0447e220659f10ed2e1dca9b
enyonyiabemba94-eng <enyonyiabemba94@gmail.com>
2026-09-06 20:42:58 +0300

    Add AFRN Android WebView launcher

---
+++ android-app/app/src/main/java/tz/afrn/footballmanagement/MainActivity.java
@@ -0,0 +1,41 @@
+package tz.afrn.footballmanagement;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebChromeClient;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.view.View;
+
+public class MainActivity extends Activity {
+    private WebView webView;
+    private static final String APP_URL = "https://enyonyiabemba94-eng.github.io/AFRN-FOOTBALL-MANAGEMENT/";
+
+    @Override protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        webView = new WebView(this);
+        setContentView(webView);
+        WebSettings s = webView.getSettings();
+        s.setJavaScriptEnabled(true);
+        s.setDomStorageEnabled(true);
+        s.setDatabaseEnabled(true);
+        s.setAllowFileAccess(false);
+        s.setAllowContentAccess(false);
+        s.setMixedContentMode(WebSettings.MIXED_CONTENT_NEVER_ALLOW);
+        s.setUserAgentString(s.getUserAgentString() + " AFRN-Android/1.0");
+        webView.setWebViewClient(new WebViewClient() {
+            @Override public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+                return false;
+            }
+        });
+        webView.setWebChromeClient(new WebChromeClient());
+        webView.setOverScrollMode(View.OVER_SCROLL_NEVER);
+        webView.loadUrl(APP_URL);
+    }
+
+    @Override public void onBackPressed() {
+        if (webView.canGoBack()) webView.goBack(); else super.onBackPressed();
+    }
+}
